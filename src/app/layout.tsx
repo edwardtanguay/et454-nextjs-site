6475e19ceb90045ee2feef59b695df2a47d44587
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-12 11:05:54 +0100

    selected nav item styled

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={`${inter.className} bg-slate-300 p-6`}>
+			<body suppressHydrationWarning={true}  className={`${inter.className} bg-slate-300 p-6`}>
 				<Header/>
 				{children}
 			</body>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,27 @@
+"use client";
 import Link from "next/link"
+import { useSelectedLayoutSegment } from "next/navigation";
 
 export const Nav = () => {
+	const activeSegment = useSelectedLayoutSegment();
+	
 	return (
 		<nav>
 			<ul className="flex gap-4 bg-slate-700 text-white p-2 font-semibold rounded mb-4">
 				<li>
-					<Link href="/">Welcome</Link>
+					<Link href="/"
+					className={activeSegment === '(welcome)' ? "text-yellow-200" : ""}
+					>Welcome</Link>
 				</li>
 				<li>
-					<Link href="/techskills">Tech Skills</Link>
+					<Link href="/techskills"
+					className={activeSegment === 'techskills' ? "text-yellow-200" : ""}
+					>Tech Skills</Link>
 				</li>
 				<li>
-					<Link href="/techbooks">Tech Books</Link>
+					<Link href="/techbooks"
+					className={activeSegment === 'techbooks' ? "text-yellow-200" : ""}
+					>Tech Books</Link>
 				</li>
 			</ul>
 		</nav>
