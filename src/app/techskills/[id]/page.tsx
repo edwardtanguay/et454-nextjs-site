b75cba6cece35dbea4740e21bf7761d2e031744a
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-12 11:52:25 +0100

    tech skills works

--- src/app/techskills/[id]/page.tsx
+++ src/app/techskills/[id]/page.tsx
@@ -9,11 +9,17 @@ export default function PageTechSkillsItem({
 }) {
 	const { techSkills } = useContext(AppContext);
 	const { id } = params;
-	const techSkill = techSkills.find(m => String(m.id) === id);
+	const techSkill = techSkills.find((m) => String(m.id) === id);
 
 	return (
 		<>
-			<p>single tech skill {techSkill?.name}</p>
+			{techSkill && (
+				<div>
+					<p className="font-bold">{techSkill.name}</p>
+					<p className="italic">{techSkill.description}</p>
+					<p><a className="underline" target="_blank" href={techSkill.url}>more info</a></p>
+				</div>
+			)}
 		</>
 	);
 }
