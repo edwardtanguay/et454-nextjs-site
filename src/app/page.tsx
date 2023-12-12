51e337bc04a4203dcb0e3c211ef4456f8304d983
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-12 10:06:12 +0100

    site styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-    <main>
-      <h1>Tech Site</h1>
-      <p>Welcome to this site.</p>
-    </main>
+		<main>
+			<h1 className="text-3xl mb-4">Tech Site</h1>
+			<p>Welcome to this site.</p>
+		</main>
 	);
 }
