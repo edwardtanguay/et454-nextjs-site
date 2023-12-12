6c9add07de037a9f43c54462836523f94099a625
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-12 10:51:06 +0100

    base nav works

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import { Nav } from "./Nav";
+
 export const Header = () => {
 	return (
 		<>
 			<h1 className="text-3xl mb-4">The Tech Site</h1>
+			<Nav/>
 		</>
 	);
 };
---
+++ src/components/Nav.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export const Nav = () => {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<Link href="/">Welcome</Link>
+				</li>
+			</ul>
+		</nav>
+	)
+}
