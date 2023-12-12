7e6ff6bcc52b13ca05b817e75887ed3748be5d17
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-12 11:47:23 +0100

    showing skill

--- src/app/techskills/[id]/page.tsx
+++ src/app/techskills/[id]/page.tsx
@@ -1,13 +1,19 @@
+"use client";
+import { AppContext } from "@/AppContext";
+import { useContext } from "react";
+
 export default function PageTechSkillsItem({
 	params,
 }: {
 	params: { id: string };
 }) {
+	const { techSkills } = useContext(AppContext);
 	const { id } = params;
+	const techSkill = techSkills.find(m => String(m.id) === id);
 
 	return (
 		<>
-			<p>single tech skill is {id}</p>
+			<p>single tech skill {techSkill?.name}</p>
 		</>
 	);
 }
