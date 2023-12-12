7cfe16d94ac3a07d5caa6d82d5b9d7fff805bda7
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-12 10:52:10 +0100

    nav works on all pages

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,6 +7,12 @@ export const Nav = () => {
 				<li>
 					<Link href="/">Welcome</Link>
 				</li>
+				<li>
+					<Link href="/techskills">Tech Skills</Link>
+				</li>
+				<li>
+					<Link href="/techbooks">Tech Books</Link>
+				</li>
 			</ul>
 		</nav>
 	)
